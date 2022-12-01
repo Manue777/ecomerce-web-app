257575549d98575def8466231dd230852960eed5
manue777 <lejuene.manu@gmail.com>
2022-12-01 02:42:15 -0500

    ading style and rootes

--- src/commom/header/Header.jsx
+++ src/commom/header/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react"
+import "./Header.css"
+import Head from "./Head"
+import Search from "./Search"
+import Navbar from "./Navbar"
 
-const Header = () => {
   return (
     <>
-    <h1>Header</h1>
+      <Head />
+      <Search CartItem/>
+      <Navbar />
     </>
-    
   )
-}
+
 
 export default Header
