6381dbd798101bbfe0927dc04b01e1e43fef2f92
manue777 <lejuene.manu@gmail.com>
2022-12-02 14:23:53 -0500

    advanced

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import Annoucne from '../components/Annoucne';
+import Categories from '../components/Categories';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NewsLetter from '../components/NewsLetter';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div>
+      <Annoucne/>
+      <Navbar/>
+      <Slider/>
+      <Categories/>
+      <Products/>
+      <NewsLetter/>
+      <Footer/>
+  </div>;
+};
 
-export default Home
+export default Home;
