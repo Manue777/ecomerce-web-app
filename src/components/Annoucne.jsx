ff59709f7fda5ed63965b9db2dcbeab41e83ffe4
manue777 <lejuene.manu@gmail.com>
2022-12-02 19:24:55 -0500

    Nabvar, announcne

--- src/components/Annoucne.jsx
+++ src/components/Annoucne.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import {Close} from "@mui/icons-material";
+import {React ,useState } from 'react';
 
-const Annoucne = () => {
-  return <div className='bg-[#8a4af3] font-bold text-white flex items-center justify-center'>
-      Hurry up 50% off Sale!
+function Announce ()  {
+
+  const [announceStyle, setAnnounceStyle]= useState 
+  ('bg-[#8a4af2] font-bold text-white flex items-center justify-center') 
+
+
+    const handleClose= () => {
+      setAnnounceStyle(announceStyle + " hidden")}
+
+  return <div className={announceStyle}>
+
+      <h2>Hurry up 50% off Sale!</h2>
+      <Close className="cursor-pointer" onclick= {handleClose}/>
   </div>;
 };
 
-export default Annoucne;
+export default Announce;
